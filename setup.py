9c9384bf16a51e90a72addbbe7be90a5d6d18799
Sytwu <121273046+Sytwu@users.noreply.github.com>
2026-01-16 02:14:18 +0000

    fix: Include scripts and configs in package for HF Space

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="ordinalclip",
-    packages=find_packages(),
+    packages=find_packages() + ["scripts", "configs"],
+    package_data={
+        "configs": ["*.yaml"],
+        "scripts": ["*.py"],
+    },
+    include_package_data=True,
 )
